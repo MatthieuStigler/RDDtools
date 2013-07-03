775bab6fe568e42908e9c398b3af6ff13e45064a
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2013-07-03 11:22:23 +0200

    as.npreg: update the RDDcoef() for allCo, still not working for all paras....

--- RDDtools/R/as.npreg.R
+++ RDDtools/R/as.npreg.R
@@ -50,7 +50,7 @@ as.npregbw_low <- function(x, npreg=FALSE,...){
 
 ## Specify inputs to npregbw:
   x <- dat$x
-  dat_np <- data.frame(y=dat$y, x=x,  D=ifelse(x>=0,1,0), Dx=ifelse(x>=0,x,0))
+  dat_np <- data.frame(y=dat$y, x=ifelse(x>=cutpoint,0,x),  D=ifelse(x>=cutpoint,1,0), Dx=ifelse(x>=cutpoint,x,0))
   range.x <- range(dat$x, na.rm=TRUE)
   bws <- c(bw, rep(9999*diff(range.x),2))
   dataPoints <- data.frame(x=c(cutpoint,cutpoint), D=c(0,1), Dx=c(0,cutpoint))
@@ -66,13 +66,13 @@ as.npregbw_low <- function(x, npreg=FALSE,...){
     res <- npreg(res, newdata=dataPoints, gradients=TRUE,...)
     class(res) <- c("RDDreg_npreg", class(res))
   }
-
+  attr(res, "RDDdf") <- dat_np
   res
 }
 
 
 #' @S3method RDDcoef RDDreg_npreg
-RDDcoef.RDDreg_npreg <- function(object, allInfo=FALSE, ...){
+RDDcoef.RDDreg_npreg <- function(object, allInfo=FALSE, allCo=FALSE, ...){
 
   co <- diff(object$mean)
   if(allInfo) {
@@ -85,6 +85,20 @@ RDDcoef.RDDreg_npreg <- function(object, allInfo=FALSE, ...){
   } else {
     res <- co
   }
+
+  if(allCo){
+    cos <- c(object$mean[1], object$grad)
+    ses <- c(object$merr[1], object$gerr)
+    zvals <- cos/ses
+    pvals <- 2 * pnorm(abs(zvals), lower.tail = FALSE)
+    res <- data.frame("Estimate"   = c(cos[1], co, cos[2:3]),
+		      "Std. Error" = c(ses[1], se, ses[2:3]),
+		      "z value"    = c(zvals[1], zval, zvals[2:3]),
+		      "Pr(>|z|)"   = c(pvals[1], pval, pvals[2:3])
+		      )
+    rownames(res) <- c("(Intercept)", "D", "x_left", "x_right")
+  }
+
   res 
 }
 
@@ -106,10 +120,12 @@ RDDcoef(reg_nonpara_npbw)
 class(reg_nonpara_np)
 RDDcoef(reg_nonpara_np)
 RDDcoef(reg_nonpara_np, allInfo=TRUE)
+RDDcoef(reg_nonpara_np, allInfo=TRUE, allCo=TRUE)
 
 ## compare:
   bw_lm <- dnorm(Lee2008_rdd$x, sd=RDDtools:::getBW(reg_nonpara))
   reg_nonpara_gaus <- RDDreg_lm(RDDobject=Lee2008_rdd, w=bw_lm)
+coef(summary(reg_nonpara_gaus))
   all.equal(RDDcoef(reg_nonpara_gaus),RDDcoef(reg_nonpara_np))
 
 
