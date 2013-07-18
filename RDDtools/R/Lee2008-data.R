6ea9ada52c09670b2df618da4b2ca0db2665f2d3
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2013-07-18 15:42:04 +0200

    Lee 2008: update man page

--- RDDtools/R/Lee2008-data.R
+++ RDDtools/R/Lee2008-data.R
@@ -1,10 +1,25 @@
 #' @name Lee2008
-#' @title Dataset used in Lee 2008 
-#' @description TODO
+#' @title Dataset used in Lee (2008)
+#' @description U.S. House elections data
 #' @docType data
 #' @usage Lee2008
-#' @format a data frame hopefully
-#' @source Lee
+#' @description Dataset described used in Imbens and Kalyamaran (2012), and probably the same dataset used in Lee (2008),
+#' @format A data frame with 6558 observations and two variables:
+#' \describe{
+#' \item{x}{Vote at election t-1}
+#' \item{y}{Vote at election t}
+#' }
+#' @source Guido Imbens webpage: \url{http://scholar.harvard.edu/imbens/scholar_software/regression-discontinuity}
+#' @references Imbens, Guido and Karthik Kalyanaraman. (2012) "Optimal Bandwidth Choice for the regression discontinuity estimator," 
+#' Review of Economic Studies (2012) 79, 933-959
+#' @references   Lee, D. (2008) Randomized experiments from non-random selection in U.S. House elections, 
+#' \emph{Journal of Econometrics}, 142, 675-697
+#' @examples 
+#' data(Lee2008)
+#' RDDlee <- RDDdata(x=x, y=y, data=Lee2008, cutpoint=0)
+#' summary(RDDlee)
+#' plot(RDDlee)
+
 
 NULL
 # Lee2008 <- read.csv("/home/mat/Dropbox/HEI/rdd/Rcode/IK bandwidth/datasets/imbens_from_MATLAB.csv", header=FALSE)
