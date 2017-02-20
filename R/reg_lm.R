0a48428de1a172f739bb9d61b29c9bf40eb37697
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2017-02-19 18:40:06 -0800

    Uses now default cct bandwidth for bin plot

--- R/reg_lm.R
+++ R/reg_lm.R
@@ -137,8 +137,14 @@ print.rdd_reg_lm <- function(x, ...) {
 
 
 #' @export
-plot.rdd_reg_lm <- function(x, ...) {
+plot.rdd_reg_lm <- function(x, binwidth=NULL, ...) {
     
+  ## set default binwitdh
+  if(is.null(binwidth)) {
+    bw_plot <- rdd_bw_cct_plot(x)
+    binwidth <- bw_plot$results["Bin Length",, drop=TRUE]
+  }
+  
     ## data
     dat <- getOriginalData(x)
     subw <- if (!is.null(x$weights)) 
@@ -146,6 +152,6 @@ plot.rdd_reg_lm <- function(x, ...) {
     pred <- data.frame(x = dat$x, y = fitted(x))[subw, ]
     
     ## plot
-    plotBin(dat$x, dat$y, h=0.05, cutpoint=getCutpoint(x), ...)
+    plotBin(dat$x, dat$y, h=0.05, cutpoint=getCutpoint(x), binwidth=binwidth, ...)
     lines(pred[order(pred$x), ])
 } 
